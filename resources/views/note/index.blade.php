8e0872428601a5f7949d61e4a67278fc299dc191
blcoussama <belcadioussama.eng@gmail.com>
2025-06-29 12:49:11 +0100

    Fixed the dahboard route not working by redirecting to the notes route

--- resources/views/note/index.blade.php
+++ resources/views/note/index.blade.php
@@ -1,4 +1,4 @@
-<x-layout>
+<x-app-layout>
     <div class="note-container">
         <a href="{{ route('note.create') }}" class="new-note-btn">New Note</a>
         <div class="notes">
@@ -24,4 +24,4 @@
         {{ $notes->links() }}
 
     </div>
-</x-layout>
+</x-app-layout>
